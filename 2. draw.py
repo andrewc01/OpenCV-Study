51fafa0568bcad4e6d887d7b3c299ceedeec966c
andy <jhyeonc@gmail.com>
2022-05-16 23:57:00 +0900

    how to draw with open cv

---
+++ 2. draw.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+blank = np.zeros((500, 500, 3), dtype='uint8')
+cv2.imshow('Blank', blank)
+
+# 1. Paint the image a certain color
+blank[200:300, 300:400] = 0, 255, 0
+cv2.imshow('Green', blank)
+
+# 2. Draw a Rectangle
+cv2.rectangle(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=2)
+cv2.imshow('Rectangle', blank)
+
+# 3. Draw a Circle
+cv2.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=-1) # thickness=-1 will fill the shape
+cv2.imshow('Circle', blank)
+
+# 4. Draw a Line
+cv2.line(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (255, 255, 255), thickness=3)
+cv2.imshow('Line', blank)
+
+# 5. Write text
+cv2.putText(blank, 'Hello, I am Batman.', (100, 255), cv2.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), thickness=2)
+cv2.imshow('Text', blank)
+
+cv2.waitKey(0)
